78c2ac7aa1782e65a6fe04b3cb4f55844c0c4e0a
mark_kibo <kibochamark@gmail.com>
2023-08-17 11:01:20 +0300

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function Home(props) {
+  // console.log(props.user.name)\
+  const h1Color=props.user.color
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+      <h1 style={{ color: h1Color }}>
+        {props.user.name} is a Web Developer from {props.user.city}
       </h1>
     </div>
-  );
-}
+  );}
+
 
 export default Home;
